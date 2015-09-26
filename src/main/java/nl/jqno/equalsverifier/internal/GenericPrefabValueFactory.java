86351b62f5890786c17be0173201e09738f76d73
Jan Ouwens <jan.ouwens@gmail.com>
2015-09-26 16:08:53 +0200

    Issue 84: Introduce GenericPrefabValueFactory

---
+++ src/main/java/nl/jqno/equalsverifier/internal/GenericPrefabValueFactory.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2015 Jan Ouwens
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.jqno.equalsverifier.internal;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Creates instances of generic types for use as prefab value.
+ *
+ * @param <T> The type to instantiate.
+ */
+public interface GenericPrefabValueFactory<T> {
+    /**
+     * Returns an empty instance of T, to be filled in by
+     * {@link #createRed(TypeTag, PrefabValues)} and
+     * {@link #createBlack(TypeTag, PrefabValues)}.
+     */
+    public T createEmpty();
+
+    /**
+     * Calls {@link #createEmpty()} and fills it in with "red" values for use
+     * as prefab value.
+     *
+     * @param typeTag Tag for the type to instantiate.
+     * @param prefabValues From which to draw object instances.
+     * @return A "red" instance of {@link T}.
+     */
+    public T createRed(TypeTag typeTag, PrefabValues prefabValues);
+
+    /**
+     * Calls {@link #createEmpty()} and fills it in with "black" values for use
+     * as prefab value.
+     *
+     * @param typeTag Tag for the type to instantiate.
+     * @param prefabValues From which to draw object instances.
+     * @return A "black" instance of {@link T}.
+     */
+    public T createBlack(TypeTag typeTag, PrefabValues prefabValues);
+
+    /**
+     * Can create instances of {@link Collection}.
+     *
+     * Must be overridden, to implement {@link #createEmpty()}.
+     *
+     * @param <T> The type of the Collection itself.
+     */
+    public abstract class CollectionPrefabValueFactory<T extends Collection> implements GenericPrefabValueFactory<T> {
+        @Override
+        public T createRed(TypeTag typeTag, PrefabValues prefabValues) {
+            T result = createEmpty();
+            Object entry = prefabValues.getRed(typeTag.getGenericTypes().get(0));
+            result.add(entry);
+            return result;
+        }
+
+        @Override
+        public T createBlack(TypeTag typeTag, PrefabValues prefabValues) {
+            T result = createEmpty();
+            Object entry = prefabValues.getBlack(typeTag.getGenericTypes().get(0));
+            result.add(entry);
+            return result;
+        }
+    }
+
+    /**
+     * Can create instances of {@link Map}.
+     *
+     * Must be overridden, to implement {@link #createEmpty()}.
+     *
+     * @param <T> The type of the Map itself.
+     */
+    public abstract class MapPrefabValueFactory<T extends Map> implements GenericPrefabValueFactory<T> {
+        @Override
+        public T createRed(TypeTag typeTag, PrefabValues prefabValues) {
+            T result = createEmpty();
+            Object key = prefabValues.getRed(typeTag.getGenericTypes().get(0));
+            Object value = prefabValues.getRed(typeTag.getGenericTypes().get(1));
+            result.put(key, value);
+            return result;
+        }
+
+        @Override
+        public T createBlack(TypeTag typeTag, PrefabValues prefabValues) {
+            T result = createEmpty();
+            Object key = prefabValues.getBlack(typeTag.getGenericTypes().get(0));
+            Object value = prefabValues.getBlack(typeTag.getGenericTypes().get(1));
+            result.put(key, value);
+            return result;
+        }
+    }
+}
---
+++ src/test/java/nl/jqno/equalsverifier/internal/GenericPrefabValueFactoryTest.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2015 Jan Ouwens
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.jqno.equalsverifier.internal;
+
+import nl.jqno.equalsverifier.JavaApiPrefabValues;
+import nl.jqno.equalsverifier.internal.GenericPrefabValueFactory.CollectionPrefabValueFactory;
+import nl.jqno.equalsverifier.internal.GenericPrefabValueFactory.MapPrefabValueFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class GenericPrefabValueFactoryTest {
+    private static final TypeTag STRING_TYPETAG = new TypeTag(String.class);
+
+    private static final TypeTag STRINGLIST_TYPETAG = new TypeTag(List.class, STRING_TYPETAG);
+    private static final GenericPrefabValueFactory<List<String>> STRINGLIST_FACTORY = new StubListPrefabValueFactory();
+
+    private static final TypeTag STRINGSET_TYPETAG = new TypeTag(Set.class, STRING_TYPETAG);
+    private static final GenericPrefabValueFactory<Set<String>> STRINGSET_FACTORY = new StubSetPrefabValueFactory();
+
+    private static final TypeTag STRINGSTRINGMAP_TYPETAG = new TypeTag(Map.class, STRING_TYPETAG, STRING_TYPETAG);
+    private static final GenericPrefabValueFactory<Map<String, String>> STRINGSTRINGMAP_FACTORY = new StubMapPrefabValueFactory();
+
+    private final PrefabValues prefabValues = new PrefabValues();
+    private String red;
+    private String black;
+
+    @Before
+    public void setUp() {
+        JavaApiPrefabValues.addTo(prefabValues);
+        red = prefabValues.getRed(STRING_TYPETAG);
+        black = prefabValues.getBlack(STRING_TYPETAG);
+    }
+
+    @Test
+    public void createRedListOfString() {
+        List<String> expected = new ArrayList<>();
+        expected.add(red);
+
+        List<String> actual = STRINGLIST_FACTORY.createRed(STRINGLIST_TYPETAG, prefabValues);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void createBlackListOfString() {
+        List<String> expected = new ArrayList<>();
+        expected.add(black);
+
+        List<String> actual = STRINGLIST_FACTORY.createBlack(STRINGLIST_TYPETAG, prefabValues);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void createRedSetOfString() {
+        Set<String> expected = new HashSet<>();
+        expected.add(red);
+
+        Set<String> actual = STRINGSET_FACTORY.createRed(STRINGSET_TYPETAG, prefabValues);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void createBlackSetOfString() {
+        Set<String> expected = new HashSet<>();
+        expected.add(black);
+
+        Set<String> actual = STRINGSET_FACTORY.createBlack(STRINGSET_TYPETAG, prefabValues);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void createRedMapOfStringToString() {
+        Map<String, String> expected = new HashMap<>();
+        expected.put(red, red);
+
+        Map<String, String> actual = STRINGSTRINGMAP_FACTORY.createRed(STRINGSTRINGMAP_TYPETAG, prefabValues);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void createBlackMapOfStringToString() {
+        Map<String, String> expected = new HashMap<>();
+        expected.put(black, black);
+
+        Map<String, String> actual = STRINGSTRINGMAP_FACTORY.createBlack(STRINGSTRINGMAP_TYPETAG, prefabValues);
+        assertEquals(expected, actual);
+    }
+
+    private static class StubListPrefabValueFactory extends CollectionPrefabValueFactory<List<String>> {
+        @Override
+        public List<String> createEmpty() {
+            return new ArrayList<>();
+        }
+    }
+
+    private static class StubSetPrefabValueFactory extends CollectionPrefabValueFactory<Set<String>> {
+        @Override
+        public Set<String> createEmpty() {
+            return new HashSet<>();
+        }
+    }
+
+    private static class StubMapPrefabValueFactory extends MapPrefabValueFactory<Map<String, String>> {
+        @Override
+        public Map<String, String> createEmpty() {
+            return new HashMap<>();
+        }
+    }
+}
